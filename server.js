cdf8bff7b13a83e379bd9929f3e55b8aaa4cef75
Submits <101422599+Submits@users.noreply.github.com>
2022-05-01 01:45:19 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const fetch = require("cross-fetch")
+
+app.get('/:username', async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var response = await fetch("https://tiktok.com/@" + req.params.username);
+    const data = await response.text()
+    res.send({html: data})
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Ready")
+})
